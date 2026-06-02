2dd0b89663b6914f1cc7eb0d988781602e287b4b
Avnishka Bhardwaj <avnishkaabhardwaj04@gmail.com>
2026-06-02 14:15:40 +0530

    selection_sort descending

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,4 @@
-# self try
+# self try ascending order
 
 new = [5, 7, 8, 4, 1, 6, 9, 2]
 # for i in range(len(nums)):
@@ -15,16 +15,31 @@ new = [5, 7, 8, 4, 1, 6, 9, 2]
 # print(nums)
 
 
-# by recursion
-def selection_sort(nums):
+# by recursion ascending order
+# def selection_sort(nums):
+#     n = len(nums)
+#     for i in range(0, n):
+#         min_index = i
+#         for j in range(i + 1, n):
+#             if nums[j] < nums[min_index]:
+#                 min_index = j
+#         nums[i], nums[min_index] = nums[min_index], nums[i]
+#     return nums
+
+
+# print(selection_sort(new))
+
+
+# descending  order try
+def selection_sort_desc(nums):
     n = len(nums)
     for i in range(0, n):
-        min_index = i
+        max_index = i
         for j in range(i + 1, n):
-            if nums[j] < nums[min_index]:
-                min_index = j
-        nums[i], nums[min_index] = nums[min_index], nums[i]
+            if nums[j] > nums[max_index]:
+                max_index = j
+        nums[i], nums[max_index] = nums[max_index], nums[i]
     return nums
 
 
-print(selection_sort(new))
+print(selection_sort_desc(new))
