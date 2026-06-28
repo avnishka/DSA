93e199502df8aede45ccc0114345161baad8fb73
Avnishka Bhardwaj <avnishkaabhardwaj04@gmail.com>
2026-06-28 13:47:39 +0530

    min_in_sorted_array

--- min_in_sorted_rotated.py
+++ min_in_sorted_rotated.py
@@ -1,4 +1,4 @@
-def findMin(self, nums: List[int]) -> int:
+def findMini(nums):
     low = 0
     high = len(nums) - 1
     mini = float("inf")
@@ -11,3 +11,23 @@ def findMin(self, nums: List[int]) -> int:
             high = mid - 1
             mini = min(nums[low], mini, nums[mid])
     return mini
+
+
+def findMin(nums):
+    low = 0
+    high = len(nums) - 1
+    mini = float("inf")
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] <= nums[high]:
+            mini = min(mini, nums[mid])
+            high = mid - 1
+        else:
+            mini = min(mini, nums[low])
+            low = mid + 1
+    return mini
+
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+print(findMin(nums))
+print(findMini(nums))
