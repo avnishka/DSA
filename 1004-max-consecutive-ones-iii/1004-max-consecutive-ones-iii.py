995d7b501274ad3553ab46a4d042a8c96cbdc94c
Avnishka Bhardwaj <145945629+avnishka@users.noreply.github.com>
2026-07-27 09:49:09 +0530

    Time: 59 ms (57.86%), Space: 22.1 MB (85.02%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -8,11 +8,11 @@ class Solution:
         while right<n:
             if nums[right]==0:
                 zeroes+=1
-            while zeroes>k:
+            if zeroes>k:
                 if nums[left]==0:
                     zeroes-=1
                 left+=1
-            if zeroes<=k:
-                maxi=max(maxi,right-left+1)
+           
+            maxi=max(maxi,right-left+1)
             right+=1
         return maxi
