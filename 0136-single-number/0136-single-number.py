b32db5ea75ae3a66a3775aa23b5475dc2bb2b7de
Avnishka Bhardwaj <145945629+avnishka@users.noreply.github.com>
2026-08-12 19:19:48 +0530

    Time: 0 ms (100%), Space: 21 MB (93.4%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        my_dict=dict()
+        ans=0
         for num in nums:
-            my_dict[num]=my_dict.get(num,0)+1
-        for k, v in my_dict.items():
-            if v == 1:
-                return k
+            ans=ans^num
+        return ans
